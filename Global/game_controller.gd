7ac7578accce4c51a1f53e8fc83a85c77cef1d97
João Lucas Silva Machado <joaolucas.silva@estudante.ufjf.br>
2025-11-04 17:26:53 -0300

    Game controller and camera tweaks

--- Global/game_controller.gd
+++ Global/game_controller.gd
@@ -1,11 +1,14 @@
 extends Node
 
+@export var mouse_sensibility_x: float = 0.2
+@export var mouse_sensibility_y: float = 0.2
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var master_volume: float = 1.0
+@export var fov: float = 75.0
 
+func set_mouse_sensibility(x: float, y: float) -> void:
+	mouse_sensibility_x = x
+	mouse_sensibility_y = y
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func get_mouse_sensibility() -> Vector2:
+	return Vector2(mouse_sensibility_x, mouse_sensibility_y)
--- Scenes/Player/camera_script.gd
+++ Scenes/Player/camera_script.gd
@@ -1,8 +1,6 @@
 extends Node3D
 class_name CameraObject
 
-@export var x_sensibility: float = 0.2
-@export var y_sensibility: float = 0.2
 @export_range(-89.0, 89.0) var min_pitch: float = -70.0
 @export_range(-89.0, 89.0) var max_pitch: float = 70.0
 
@@ -12,8 +10,20 @@ class_name CameraObject
 var pitch: float = 0.0
 var mouse_free: bool = false
 
+var x_sensibility: float
+var y_sensibility: float
+
 func _ready() -> void:
+	update_sensibility_and_fov()
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func update_sensibility_and_fov() -> void:
+	# Essa função serve pra no futuro, sempre que atualizar no menu a sensibilidade e FOV
+	# chamar essa função via signal. Tive problemas com sensibilidade em outra jam que participei
+	
+	x_sensibility = GameController.mouse_sensibility_x
+	y_sensibility = GameController.mouse_sensibility_y
+	camera.fov = GameController.fov
 
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -26,6 +36,5 @@ func _unhandled_input(event: InputEvent) -> void:
 
 	if not mouse_free and event is InputEventMouseMotion:
 		player.rotate_y(-deg_to_rad(event.relative.x * x_sensibility))
-		
 		pitch = clamp(pitch - event.relative.y * y_sensibility, min_pitch, max_pitch)
 		camera.rotation_degrees.x = pitch
