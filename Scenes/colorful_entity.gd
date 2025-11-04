aa23b68a4fdbe2b3720ef578828eda4b8c8c3ee9
João Lucas Silva Machado <joaolucas.silva@estudante.ufjf.br>
2025-11-04 12:42:28 -0300

    Adds is_active feature to improve debugging and support future enemy related functionality

--- Scenes/colorful_entity.gd
+++ Scenes/colorful_entity.gd
@@ -2,7 +2,7 @@ extends Node3D
 class_name ColorfulEntity
 
 @export var color: GameManager.ColorEnum
-
+var is_active: bool = false
 
 func _ready() -> void:
 	GameManager.color_changed.connect(_on_color_changed)
@@ -10,4 +10,8 @@ func _ready() -> void:
 
 
 func _on_color_changed(active_color: GameManager.ColorEnum) -> void:
-	visible = (color == active_color)
+	is_active = (color == active_color)
+	_set_active(is_active)
+
+func _set_active(active: bool) -> void:
+	is_active = active 
--- Scenes/colorful_tree.gd
+++ Scenes/colorful_tree.gd
@@ -1,22 +1,16 @@
 extends ColorfulEntity
-class_name ColorfulObject
+class_name ColorfulTree
 
+func _set_active(active: bool) -> void:
+	is_active = active 
+	_update_recursive(self)
 
-func _on_color_changed(active_color: GameManager.ColorEnum) -> void:
-	var is_active = (color == active_color)
-	_update_recursive(self, is_active)
-
-
-func _update_recursive(node: Node, visible_state: bool) -> void:
-	node.visible = visible_state
+func _update_recursive(node: Node) -> void:
+	node.visible = is_active
 
 	if node.has_method("set_collision_layer") and node.has_method("set_collision_mask"):
-		if visible_state:
-			node.collision_layer = 1
-			node.collision_mask = 1
-		else:
-			node.collision_layer = 0
-			node.collision_mask = 0
+		node.collision_layer = 1 if is_active else 0
+		node.collision_mask = 1 if is_active else 0
 
 	for child in node.get_children():
-		_update_recursive(child, visible_state)
+		_update_recursive(child)
