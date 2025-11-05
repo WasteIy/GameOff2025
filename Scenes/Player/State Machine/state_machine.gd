1d4de0921922ce6f312e09932cc028c850a99851
João Lucas Silva Machado <joaolucas.silva@estudante.ufjf.br>
2025-11-05 16:12:16 -0300

    Modular state machine

--- Scenes/Player/State Machine/state_machine.gd
+++ Scenes/Player/State Machine/state_machine.gd
@@ -1,1 +1,44 @@
-extends Node
+class_name StateMachine extends Node
+
+@export var initial_state: State
+
+var current_state: State
+var states: Dictionary[String, State] = {}
+
+
+# Aqui adiciona todos os filhos desse nó no dictionary
+func _ready() -> void:
+	for child in get_children():
+		if child is State:
+			child.state_machine = self
+			states[child.name.to_lower()] = child
+	
+	# Se acostuma com isso, a gente sempre tem que conferir se existe
+	# um estado atual ou inicial antes de rodar o código, pro jogo não crashar
+	if initial_state:
+		initial_state.enter()
+		current_state = initial_state
+
+# A gente precisa transferir os process desse script pro estado atual
+func _process(delta: float) -> void:
+	if current_state:
+		current_state.update(delta)
+
+func _physics_process(delta: float) -> void:
+	if current_state:
+		current_state.physics_update(delta)
+
+func change_state(new_state_name: String) -> void:
+	var new_state: State = states.get(new_state_name.to_lower())
+	# Esse to_lower() é pra evitar problema envolvendo capitalização
+	
+	# Assert pega a primeira variável, se ela for true, tudo bem, se for false, dá erro
+	assert(new_state, "State not found: " + new_state_name)
+	# Ou seja, se new_state é falso, significa que a gente chamou um state que não existe
+	
+	if current_state:
+		current_state.exit()
+		
+	new_state.enter()
+	
+	current_state = new_state
--- Scenes/Player/State Machine/state_script.gd
+++ Scenes/Player/State Machine/state_script.gd
@@ -1,1 +1,15 @@
-extends Node
+class_name State extends Node
+
+var state_machine: StateMachine
+
+func enter() -> void:
+	pass
+	
+func exit() -> void:
+	pass
+	
+func update(_delta: float) -> void:
+	pass
+
+func physics_update(_delta: float) -> void:
+	pass
