f83f4adb0a3f41dd6bbb11c1c6a7c92565d7bc8d
Isabela Coelho <isabelacoelhoo@gmail.com>
2025-11-10 11:16:36 -0300

    Add pause menu

---
+++ Scenes/UI/Menu/pause_menu.gd
@@ -0,0 +1,26 @@
+extends Control
+
+var pause_toggle = false
+
+func _ready() -> void:
+	self.visible = false
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):  # Tecla ESC
+		pause_and_unpause()
+
+func pause_and_unpause():
+	pause_toggle = !pause_toggle
+	get_tree().paused = pause_toggle
+	self.visible = pause_toggle
+	
+	if pause_toggle:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _on_resume_pressed():
+	pause_and_unpause()
+
+func _on_quit_pressed():
+	get_tree().quit()
