59496c09a98936155d84bb995554ec22684ba72b
João Lucas Silva Machado <joaolucas.silva@estudante.ufjf.br>
2025-11-10 22:09:34 -0300

    New standard variable value

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,11 +1,11 @@
 class_name Player extends CharacterBody3D
 
 @export_group("Movement Variables")
-@export var move_speed : float = 10.0
-@export var max_speed : float = 15.0
-@export var move_acceleration : float = 10.0
+@export var move_speed : float = 9.0
+@export var max_speed : float = 50.0
+@export var move_acceleration : float = 11.0
 @export var move_deceleration : float = 10.0
-@export var bunny_hop_desired_move_speed_increase : float = 1.0
+@export var bunny_hop_desired_move_speed_increase : float = 3.0
 @export var desired_move_speed_curve : Curve
 @export var in_air_move_speed_curve : Curve
 var desired_move_speed : float = 0.0
@@ -14,13 +14,13 @@ var last_frame_position : Vector3
 var last_frame_velocity : Vector3
 
 @export_group("Jump Variables")
-@export var jump_height: float = 4.5
+@export var jump_height: float = 2.1
 @export var number_air_jump : int = 1
-@export var coyote_jump_cooldown : float = 0.2
-@export var in_air_input_multiplier : float = 0.8
-@export var jump_time_to_peak : float = 0.4
-@export var jump_time_to_fall : float = 0.35
-@export var hit_ground_cooldown : float = 1.0 # Quantidade de tempo que se mantém a velocidade acumulada antes de perder ela
+@export var coyote_jump_cooldown : float = 0.3
+@export var in_air_input_multiplier : float = 1
+@export var jump_time_to_peak : float = 0.34
+@export var jump_time_to_fall : float = 0.28
+@export var hit_ground_cooldown : float = 0.2 # Quantidade de tempo que se mantém a velocidade acumulada antes de perder ela
 @onready var jump_velocity : float = (2.0 * jump_height) / jump_time_to_peak
 var number_air_jump_reference: int
 var buffered_jump_on : bool
