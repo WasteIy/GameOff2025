eb6b688071e8e8af28cf6e082609c2185b1a9e8e
Isabela Coelho <isabelacoelhoo@gmail.com>
2025-11-16 10:13:42 -0300

    Change settings menu

--- Scenes/UI/Menu/main_menu.gd
+++ Scenes/UI/Menu/main_menu.gd
@@ -6,3 +6,7 @@ func _on_start_pressed() -> void:
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/UI/Menu/settings.tscn")
--- Scenes/UI/Menu/pause_menu.gd
+++ Scenes/UI/Menu/pause_menu.gd
@@ -1,9 +1,15 @@
 extends Control
 
+@onready var settings: Control = $Settings
+@onready var resolutions: OptionButton = $Settings/Resolutions
+@onready var pause_menu_container: VBoxContainer = $VBoxContainer
+
 var pause_toggle = false
+var settings_toggle = false
 
 func _ready() -> void:
 	self.visible = false
+	settings.visible = false
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):  # Tecla ESC
@@ -24,3 +30,25 @@ func _on_resume_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+func _on_settings_pressed() -> void:
+	$Settings.show()
+	pause_menu_container.hide()
+	
+func _on_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(0, value)
+
+func _on_resolutions_item_selected(index: int) -> void:
+	var resolution: String = resolutions.get_item_text(index)
+	var h_resolution: int = int(resolution.get_slice("x", 0))
+	var v_resolution: int = int(resolution.get_slice("x", 1))
+	DisplayServer.window_set_size(Vector2i(h_resolution, v_resolution))
+			
+
+func _on_mute_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_volume_db(0,toggled_on)
+
+
+func _on_back_pressed() -> void:
+	$Settings.hide()
+	pause_menu_container.show()
