f4db83a18b076c3e380caf35a36a824a7ac9c102
João Lucas Silva Machado <joaolucas.silva@estudante.ufjf.br>
2025-11-17 18:44:40 -0300

    Fixes

--- Scenes/UI/Menu/main_menu.gd
+++ Scenes/UI/Menu/main_menu.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var v_box_container: VBoxContainer = $VBoxContainer
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Levels/test_level.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Levels/debug_level.tscn")
 
 
 func _on_exit_pressed() -> void:
