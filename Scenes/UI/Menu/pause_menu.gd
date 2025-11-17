3db17f972307e79d8adc506bd0210474b6ef7b4d
Isabela Coelho <isabelacoelhoo@gmail.com>
2025-11-17 17:07:38 -0300

    Fix resolutions bug

--- Scenes/UI/Menu/pause_menu.gd
+++ Scenes/UI/Menu/pause_menu.gd
@@ -6,15 +6,18 @@ extends Control
 
 var pause_toggle = false
 var settings_toggle = false
-static var resolucoes = {
-  "Full HD": Vector2i(1920, 1080),
-  "HD" : Vector2i(1600, 900),
+static var resolutionsAux = {
+	"Normal" : Vector2i(1152,648),
+	"Full HD": Vector2i(1920, 1080),
+	"HD" : Vector2i(1600, 900),
+	"Test" : Vector2i(1100,680),
+	"Mid" : Vector2i(800,450)
 }
 
 func _ready() -> void:
 	self.visible = false
 	settings.visible = false
-	for chave in resolucoes:
+	for chave in resolutionsAux:
 		resolutions.add_item(chave)
 	resolutions.select(0)
 
@@ -46,8 +49,9 @@ func _on_h_slider_value_changed(value: float) -> void:
 	GameController.master_volume = value
 
 func _on_resolutions_item_selected(index: int) -> void:
-	var aux: Vector2i = resolucoes.get(resolutions.get_item_text(index))
+	var aux: Vector2i = resolutionsAux.get(resolutions.get_item_text(index))
 	DisplayServer.window_set_size(aux)
+	DisplayServer.window_set_position(Vector2i(0,0))
 
 func _on_mute_toggled(toggled_on: bool) -> void:
 	GameController.master_volume = toggled_on
